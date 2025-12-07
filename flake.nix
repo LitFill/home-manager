1312b3c5ed285c8b8fa47001c78fb914a5d1d3d4
LitFill <marrazzy54@gmail.com>
2025-12-08 02:27:39 +0700

    feat(git): integrate .gitconfig to home.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,31 @@
 {
-    description = "LitFill's Home Manager flake";
+  description = "Home Manager configuration of litfill";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        home-manager.url = "github:nix-community/home-manager";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, home-manager, ... }: {
-        defaultPackage.x86_64-linux = home-manager.packages.x86_64-linux.default;
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."litfill" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-        homeConfigurations = {
-            "litfill" = home-manager.lib.homeManagerConfiguration {
-                pkgs = nixpkgs.legacyPackages.x86_64-linux;
-                modules = [ ./home.nix ];
-            };
-        };
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
     };
 }
