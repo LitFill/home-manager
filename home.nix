d2d4f9fd1ba5cbbaddef8ab2b5e74cb6ed245c8a
LitFill <marrazzy54@gmail.com>
2025-12-08 20:37:31 +0700

    feat: add various language servers

--- home.nix
+++ home.nix
@@ -18,19 +18,25 @@
     home.stateVersion = "25.11"; # Please read the comment before changing.
 
     home.packages = with pkgs; [
+        bash-language-server
         btop
         fastfetch
         haskell-language-server
+        helix
         hledger
         hledger-iadd
         hledger-ui
         hledger-utils
         hledger-web
         hlint
+        lua-language-server
         nixfmt
         pandoc
         puffin
+        rustup
         shellcheck
+        typescript-language-server
+        yaml-language-server
     ];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -46,6 +52,14 @@
         #   org.gradle.console=verbose
         #   org.gradle.daemon.idletimeout=3600000
         # '';
+
+        "stylua.toml".text = ''
+            # Konfigurasi untuk StyLua
+            column_width     = 80
+            indent_type      = "Spaces"
+            indent_width     = 4
+            call_parentheses = "None"
+        '';
     };
 
     home.sessionVariables = {
