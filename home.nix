1c488f09b38ddcc9b1310e9121756163f844fc2f
LitFill <marrazzy54@gmail.com>
2025-12-09 13:41:10 +0700

    feat: update

--- home.nix
+++ home.nix
@@ -6,6 +6,11 @@
 {
     home.username = "litfill";
     home.homeDirectory = "/home/litfill";
+    accounts.email.accounts."litfill" = {
+        primary = true;
+        realName = "LitFill";
+        address = "marrazzy54@gmail.com";
+    };
 
     imports = [
         ./config/git.nix
@@ -36,23 +41,13 @@
         rustup
         shellcheck
         typescript-language-server
+        typst
         yaml-language-server
     ];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     home.file = {
-        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-        # # symlink to the Nix store copy.
-        # ".screenrc".source = dotfiles/screenrc;
-
-        # # You can also set the file content immediately.
-        # ".gradle/gradle.properties".text = ''
-        #   org.gradle.console=verbose
-        #   org.gradle.daemon.idletimeout=3600000
-        # '';
-
         "stylua.toml".text = ''
             # Konfigurasi untuk StyLua
             column_width     = 80
@@ -64,6 +59,7 @@
 
     home.sessionVariables = {
         EDITOR = "nvim";
+        RUST_BACKTRACE = 1;
     };
 
     # Let Home Manager install and manage itself.
@@ -79,4 +75,6 @@
             nvim-treesitter.withAllGrammars
         ];
     };
+
+    home.shell.enableNushellIntegration = true;
 }
