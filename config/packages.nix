b0b26a4519176d57e465a48cdf3bc202364945f6
LitFill <marrazzy54@gmail.com>
2026-01-18 22:47:59 +0700

    feat: add several utility packages and fonts
    - Add caddy, chez, fd, glow, ripgrep, rsync
    - Add monaspace nerd font
    - Add yazi git plugin
    - Comment out idris2 in favor of pack

--- config/packages.nix
+++ config/packages.nix
@@ -1,40 +1,48 @@
 {
-    pkgs,
-    ...
+  pkgs,
+  ...
 }:
 {
-    home.packages = with pkgs; [
-        # cabal-install
-        # ghc
-        # stack
-        bash-language-server
-        btop
-        direnv
-        fastfetch
-        fourmolu
-        gum
-        haskell-language-server
-        helix
-        hledger
-        hledger-iadd
-        hledger-ui
-        hledger-utils
-        hledger-web
-        hlint
-        idris2
-        idris2Packages.idris2Lsp
-        idris2Packages.pack
-        lua-language-server
-        nh
-        nil
-        nixfmt
-        pandoc
-        puffin
-        rustup
-        shellcheck
-        tmux
-        typescript-language-server
-        typst
-        yaml-language-server
-    ];
+  home.packages = with pkgs; [
+    # cabal-install
+    # ghc
+    # stack
+    bash-language-server
+    btop
+    caddy
+    chez
+    direnv
+    fastfetch
+    fd
+    fourmolu
+    glow
+    gum
+    haskell-language-server
+    helix
+    hledger
+    hledger-iadd
+    hledger-ui
+    hledger-utils
+    hledger-web
+    hlint
+    # idris2
+    # idris2Packages.idris2Lsp
+    idris2Packages.pack
+    lua-language-server
+    nerd-fonts.monaspace
+    nh
+    nil
+    nixfmt
+    pandoc
+    puffin
+    ripgrep
+    rsync
+    rustup
+    shellcheck
+    tmux
+    typescript-language-server
+    typst
+    yaml-language-server
+    yaziPlugins.git
+  ];
 }
