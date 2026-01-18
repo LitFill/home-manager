73f2fb8ed7bb45c26367936d60e110cb50e8d4ee
LitFill <marrazzy54@gmail.com>
2026-01-18 22:48:38 +0700

    feat(nixvim): restructure LSP configuration and add rustaceanvim
    - Reformat lua_ls config for better readability
    - Add ocamllsp server
    - Configure idris2_ls to use pack instead of direct binary
    - Comment out rust_analyzer in favor of rustaceanvim
    - Add rustaceanvim plugin with comprehensive settings

--- config/nixvim/lsp.nix
+++ config/nixvim/lsp.nix
@@ -1,76 +1,97 @@
 { lib, ... }:
 {
-    lsp = {
-        inlayHints.enable = true;
-        servers.lua_ls.config = {
-            on_init = lib.nixvim.utils.mkRaw ''
-                function(client)
-                          if client.workspace_folders then
-                              local path = client.workspace_folders[1].name
-                              if
-                                  path ~= vim.fn.stdpath('config')
-                                  and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-                              then return
-                              end
-                          end
+  lsp = {
+    inlayHints.enable = true;
+    servers.lua_ls.config = {
+      on_init = lib.nixvim.utils.mkRaw ''
+        function(client)
+                  if client.workspace_folders then
+                      local path = client.workspace_folders[1].name
+                      if
+                          path ~= vim.fn.stdpath('config')
+                          and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                      then return
+                      end
+                  end
 
-                          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                              runtime = {
-                                  -- Tell the language server which version of Lua you're using (most
-                                  -- likely LuaJIT in the case of Neovim)
-                                  version = 'LuaJIT',
-                                  -- Tell the language server how to find Lua modules same way as Neovim
-                                  -- (see `:h lua-module-load`)
-                                  path = {
-                                      'lua/?.lua',
-                                      'lua/?/init.lua',
-                                  },
-                              },
-                              -- Make the server aware of Neovim runtime files
-                              workspace = {
-                                  checkThirdParty = false,
-                                  library = {
-                                      vim.env.VIMRUNTIME
-                                  }
-                              }
-                          })
-                      end'';
+                  client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                      runtime = {
+                          -- Tell the language server which version of Lua you're using (most
+                          -- likely LuaJIT in the case of Neovim)
+                          version = 'LuaJIT',
+                          -- Tell the language server how to find Lua modules same way as Neovim
+                          -- (see `:h lua-module-load`)
+                          path = {
+                              'lua/?.lua',
+                              'lua/?/init.lua',
+                          },
+                      },
+                      -- Make the server aware of Neovim runtime files
+                      workspace = {
+                          checkThirdParty = false,
+                          library = {
+                              vim.env.VIMRUNTIME
+                          }
+                      }
+                  })
+              end'';
 
-            settings = {
-                Lua = { };
-            };
-        };
+      settings = {
+        Lua = { };
+      };
     };
+  };
 
-    plugins.lsp = {
+  plugins.lsp = {
+    enable = true;
+    keymaps = {
+      silent = true;
+      lspBuf = {
+        gd = "definition";
+        gD = "declaration";
+        K = "hover";
+        gi = "implementation";
+        "<leader>rn" = "rename";
+        "<leader>ca" = "code_action";
+      };
+    };
+    servers = {
+      bashls.enable = true;
+      clangd.enable = true;
+      jsonls.enable = true;
+      lua_ls.enable = true;
+      ocamllsp.enable = true;
+      nixd.enable = true;
+      nushell.enable = true;
+      sqls.enable = true;
+
+      idris2_ls = {
         enable = true;
-        keymaps = {
-            silent = true;
-            lspBuf = {
-                gd = "definition";
-                gD = "declaration";
-                K = "hover";
-                gi = "implementation";
-                "<leader>rn" = "rename";
-                "<leader>ca" = "code_action";
-            };
+        # Memaksa LSP dijalankan melalui pack
+        config = {
+          cmd = [
+            "pack"
+            "exec"
+            "idris2-lsp"
+          ];
         };
-        servers = {
-            bashls.enable = true;
-            clangd.enable = true;
-            idris2_ls.enable = true;
-            jsonls.enable = true;
-            lua_ls.enable = true;
-            nixd.enable = true;
-            nushell.enable = true;
-            sqls.enable = true;
+      };
 
-            # Rust (Standard setup)
-            rust_analyzer = {
-                enable = true;
-                installCargo = true;
-                installRustc = true;
-            };
-        };
+      # Rust (Standard setup)
+      # rust_analyzer = {
+      #     enable = true;
+      #     installCargo = true;
+      #     installRustc = true;
+      # };
     };
+  };
+
+  plugins.rustaceanvim = {
+    enable = true;
+    settings.server.default_settings.rust-analyzer = {
+      checkOnSave = true;
+      cargo.features = "all";
+      procMacro.enable = true;
+    };
+  };
 }
