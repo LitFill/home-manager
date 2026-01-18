e75961585d6c7c3a58731c0761f7b0943f7c31a1
LitFill <marrazzy54@gmail.com>
2026-01-18 22:48:41 +0700

    feat(nixvim): add conform-nvim for code formatting
    - Configure conform-nvim with format-on-save
    - Add formatters for lua, nix, haskell, rust, cpp
    - Set up fallback to LSP formatting
    - Add whitespace and blank line cleanup

--- config/nixvim/plugins.nix
+++ config/nixvim/plugins.nix
@@ -1,141 +1,163 @@
 {
-    pkgs,
-    lib,
-    ...
+  pkgs,
+  lib,
+  ...
 }:
 {
-    plugins = {
-        mini-files = {
-            enable = true;
-            settings = {
-                windows = {
-                    preview = true;
-                    width_preview = 60;
-                };
-                options = {
-                    use_as_default_explorer = true;
-                };
-            };
+  plugins = {
+    mini-files = {
+      enable = true;
+      settings = {
+        windows = {
+          preview = true;
+          width_preview = 60;
         };
-
-        mini-ai.enable = true;
-        mini-align.enable = true;
-        mini-bracketed.enable = true;
-        mini-comment.enable = true;
-        mini-completion.enable = false;
-        mini-cursorword.enable = true;
-        mini-diff.enable = true;
-        mini-doc.enable = true;
-        mini-extra.enable = true;
-        mini-git.enable = true;
-        mini-icons.enable = true;
-        mini-indentscope.enable = true;
-        mini-jump.enable = true;
-        mini-jump2d.enable = true;
-        mini-move.enable = true;
-        mini-notify.enable = true;
-        mini-operators.enable = true;
-        mini-pairs.enable = true;
-        mini-pick.enable = true;
-        mini-splitjoin.enable = true;
-        mini-starter.enable = true;
-        mini-statusline.enable = true;
-        mini-surround.enable = true;
-        mini-tabline.enable = true;
-        mini-trailspace.enable = true;
-
-        friendly-snippets.enable = true;
-        transparent.enable = true;
-        neogit.enable = true;
-        todo-comments.enable = true;
-        origami.enable = true;
-        rainbow-delimiters.enable = true;
-
-        render-markdown = {
-            enable = true;
-            settings = {
-                code = {
-                    width = "block";
-                    right_pad = 1;
-                };
-            };
+        options = {
+          use_as_default_explorer = true;
         };
-
-        treesitter = {
-            enable = true;
-            settings = {
-                highlight = {
-                    enable = true;
-                    additional_vim_regex_highlighting = true;
-                    disable = lib.nixvim.utils.mkRaw ''
-                        function (_, buf)
-                                      local max_filesize = 1000 * 1024
-                                      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                                      if ok and stats and stats.size > max_filesize
-                                      then
-                                          return true
-                                      end
-                                  end
-                    '';
-                };
-                indent.enable = true;
-            };
-            folding.enable = true;
-
-            grammarPackages = [ pkgs.vimPlugins.nvim-treesitter.withAllGrammars ];
-        };
-
-        haskell-tools = {
-            enable = true;
-            settings = {
-                hls = {
-                    default_settings = {
-                        haskell = {
-                            formattingProvider = "fourmolu";
-                            plugin = {
-                                hlint = {
-                                    codeActionsOn = true;
-                                    diagnosticsOn = true;
-                                };
-                                importLens = {
-                                    codeActionsOn = true;
-                                    codeLensOn = true;
-                                    globalOn = true;
-                                };
-                            };
-                        };
-                    };
-                };
-            };
-        };
-
-        idris2.enable = true;
-
-        gitignore.enable = true;
-        goto-preview.enable = true;
-        indent-o-matic.enable = true;
-        haskell-scope-highlighting.enable = true;
-        helpview.enable = true;
-        hlchunk.enable = true;
-        hmts.enable = true;
-        nix.enable = true;
-
-        blink-cmp = {
-            enable = true;
-            settings = {
-                keymap.preset = "enter";
-                signature.enabled = true;
-                sources.default = [
-                    "lsp"
-                    "path"
-                    "snippets"
-                    "buffer"
-                ];
-                appearance = {
-                    use_nvim_cmp_as_default = true;
-                    nerd_font_variant = "mono";
-                };
-            };
-        };
+      };
     };
+
+    mini-ai.enable = true;
+    mini-align.enable = true;
+    mini-bracketed.enable = true;
+    mini-comment.enable = true;
+    mini-completion.enable = false;
+    mini-cursorword.enable = true;
+    mini-diff.enable = true;
+    mini-doc.enable = true;
+    mini-extra.enable = true;
+    mini-git.enable = true;
+    mini-icons.enable = true;
+    mini-indentscope.enable = true;
+    mini-jump.enable = true;
+    mini-jump2d.enable = true;
+    mini-move.enable = true;
+    mini-notify.enable = true;
+    mini-operators.enable = true;
+    mini-pairs.enable = true;
+    mini-pick.enable = true;
+    mini-splitjoin.enable = true;
+    mini-starter.enable = true;
+    mini-statusline.enable = true;
+    mini-surround.enable = true;
+    mini-tabline.enable = true;
+    mini-trailspace.enable = true;
+
+    friendly-snippets.enable = true;
+    transparent.enable = true;
+    neogit.enable = true;
+    todo-comments.enable = true;
+    origami.enable = true;
+    rainbow-delimiters.enable = true;
+
+    render-markdown = {
+      enable = true;
+      settings = {
+        code = {
+          width = "block";
+          right_pad = 1;
+        };
+      };
+    };
+
+    treesitter = {
+      enable = true;
+      settings = {
+        highlight = {
+          enable = true;
+          additional_vim_regex_highlighting = true;
+          disable = lib.nixvim.utils.mkRaw ''
+            function (_, buf)
+                          local max_filesize = 1000 * 1024
+                          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                          if ok and stats and stats.size > max_filesize
+                          then
+                              return true
+                          end
+                      end
+          '';
+        };
+        indent.enable = true;
+      };
+      folding.enable = true;
+
+      grammarPackages = [ pkgs.vimPlugins.nvim-treesitter.withAllGrammars ];
+    };
+
+    haskell-tools = {
+      enable = true;
+      settings = {
+        hls = {
+          default_settings = {
+            haskell = {
+              formattingProvider = "fourmolu";
+              plugin = {
+                hlint = {
+                  codeActionsOn = true;
+                  diagnosticsOn = true;
+                };
+                importLens = {
+                  codeActionsOn = true;
+                  codeLensOn = true;
+                  globalOn = true;
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    idris2.enable = true;
+
+    gitignore.enable = true;
+    goto-preview.enable = true;
+    indent-o-matic.enable = true;
+    haskell-scope-highlighting.enable = true;
+    helpview.enable = true;
+    hlchunk.enable = true;
+    hmts.enable = true;
+    nix.enable = true;
+
+    blink-cmp = {
+      enable = true;
+      settings = {
+        keymap.preset = "enter";
+        signature.enabled = true;
+        sources.default = [
+          "lsp"
+          "path"
+          "snippets"
+          "buffer"
+        ];
+        appearance = {
+          use_nvim_cmp_as_default = true;
+          nerd_font_variant = "mono";
+        };
+      };
+    };
+
+    conform-nvim = {
+      enable = true;
+      settings = {
+        format_on_save = {
+          lsp_fallback = true;
+          timeout_ms = 500;
+        };
+
+        formatters_by_ft = {
+          lua = [ "stylua" ];
+          nix = [ "nixfmt" ];
+          haskell = [ "fourmolu" ];
+          rust = [ "rustfmt" ];
+          cpp = [ "clang-format" ];
+          "_" = [
+            "trim_whitespace"
+            "squeeze_blanks"
+          ];
+        };
+      };
+    };
+  };
 }
