5bebcf976a814b8dc6fd9f97d1a28132b791b1c5
LitFill <marrazzy54@gmail.com>
2026-01-18 22:48:46 +0700

    chore: update flake.nix formatting

--- flake.nix
+++ flake.nix
@@ -1,40 +1,40 @@
 {
-    description = "Home Manager configuration of litfill";
+  description = "Home Manager configuration of litfill";
 
-    inputs = {
-        # Specify the source of Home Manager and Nixpkgs.
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        nixvim.url = "github:nix-community/nixvim";
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim.url = "github:nix-community/nixvim";
+  };
 
-    outputs =
-        {
-            nixpkgs,
-            home-manager,
-            nixvim,
-            ...
-        }:
-        let
-            system = "x86_64-linux";
-            pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-            homeConfigurations."litfill" = home-manager.lib.homeManagerConfiguration {
-                inherit pkgs;
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nixvim,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."litfill" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-                # Specify your home configuration modules here, for example,
-                # the path to your home.nix.
-                modules = [
-                    ./home.nix
-                    nixvim.homeModules.nixvim
-                ];
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+          nixvim.homeModules.nixvim
+        ];
 
-                # Optionally use extraSpecialArgs
-                # to pass through arguments to home.nix
-            };
-        };
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
