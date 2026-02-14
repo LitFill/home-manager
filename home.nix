ecabaf38e9aded66f17bb0a17e5c80ae269ca282
LitFill <marrazzy54@gmail.com>
2026-02-15 01:08:05 +0700

    move config files to external files and add matugen template

--- home.nix
+++ home.nix
@@ -40,40 +40,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    "stylua.toml".text = ''
-      # Konfigurasi untuk StyLua
-      column_width     = 80
-      indent_type      = "Spaces"
-      indent_width     = 4
-      call_parentheses = "None"
-    '';
-    "fourmolu.yaml".text = ''
-      # Generated from web app, for more information, see: https://fourmolu.github.io/config/
-      indentation:                4
-      column-limit:               80
-      function-arrows:            leading-args
-      comma-style:                leading
-      record-style:               aligned
-      import-export-style:        leading
-      import-grouping:            by-scope-then-qualified
-      indent-wheres:              false
-      record-brace-space:         true
-      newlines-between-decls:     2
-      haddock-style:              multi-line
-      haddock-style-module:       multi-line
-      haddock-location-signature: trailing
-      let-style:                  auto
-      in-style:                   right-align
-      if-style:                   indented
-      single-constraint-parens:   always
-      single-deriving-parens:     always
-      sort-constraints:           true
-      sort-derived-classes:       true
-      sort-deriving-clauses:      true
-      trailing-section-operators: true
-      unicode:                    never
-      respectful:                 true
-    '';
+    ".config/nvim/lua/matugen-template.lua".source = ./files/matugen-template.lua;
+    "stylua.toml".source = ./files/stylua.toml;
+    "fourmolu.yaml".source = ./files/fourmolu.yaml;
     ".ocamlinit".text = ''
       #require "core.top";;
       #require "ppx_jane";;
