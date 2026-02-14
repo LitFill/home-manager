a44584e458a66b61b350da8242764adf80608501
LitFill <marrazzy54@gmail.com>
2026-02-15 01:40:45 +0700

    feat(nixvim): refactor nixvim.nix to import base.nix

--- config/nixvim.nix
+++ config/nixvim.nix
@@ -6,14 +6,7 @@
     viAlias = true;
     vimAlias = true;
 
-    imports = [
-      ({ config, lib, ... }: import ./nixvim/options.nix { inherit config lib; })
-      ./nixvim/lsp.nix
-      ./nixvim/plugins.nix
-      ./nixvim/keymaps.nix
-      ./nixvim/autocmds.nix
-      ./nixvim/extra.nix
-    ];
+    imports = [ ./nixvim/base.nix ];
 
     filetype.extension.ua = "uiua";
   };
